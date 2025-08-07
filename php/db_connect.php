7d560f540ba5b4e5fce5f0d204b9d020e8738edc
Jacke1012 <jacob.jonasson2003@gmail.com>
2025-08-07 16:07:59 +0200

    lol

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,17 +1,40 @@
 <?php
-$servername = "localhost";
-$username = "financeUser";
-$password = "financeUser";
-$dbname = "finance";
+//$servername = "localhost";
+//$username = "financeUser";
+//$password = "financeUser";
+//$dbname = "finance";
 
 // Create connection
 //$conn = new mysqli($servername, $username, $password, $dbname);
 
-$conn = new mysqli("mariadb", "user", "pass", "database");
+//$conn = new mysqli("mariadb", "user", "pass", "database");
+
+$host = getenv('DB_HOST') ?: 'localhost';
+$db = getenv('DB_NAME') ?: 'finance';
+$user = getenv('DB_USER') ?: 'financeuser';
+$pass = getenv('DB_PASS') ?: 'supersecret';
+
+$conn = new mysqli($host, $user, $pass, $db);
 
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+$createExpensesTable = "
+CREATE TABLE IF NOT EXISTS expenses (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  date_time DATETIME NOT NULL,
+  amount DECIMAL(10,2) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+";
+
+if ($conn->query($createExpensesTable) === FALSE) {
+    die("Error creating expenses table: " . $conn->error);
+}
+
+
 ?>
