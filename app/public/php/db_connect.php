aa1310abcb6945e73de6407727c6078faa7ec058
Jacke1012 <jacob.jonasson2003@gmail.com>
2025-08-17 19:53:20 +0200

    sad

--- app/public/php/db_connect.php
+++ app/public/php/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header("Access-Control-Allow-Origin: *");
 //$servername = "localhost";
 //$username = "financeUser";
 //$password = "financeUser";
