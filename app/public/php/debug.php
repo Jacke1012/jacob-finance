942ee8db679957d477a58ea33f0154504126d86b
Jacke1012 <jacob.jonasson2003@gmail.com>
2025-09-03 09:41:40 +0200

    debug

--- app/public/php/debug.php
+++ app/public/php/debug.php
@@ -5,4 +5,12 @@ echo "DB_NAME=" . getenv('DB_NAME') . "\n";
 //echo "DB_USER=" . getenv('DB_USER') . "\n";
 //echo "DB_PASS=" . getenv('DB_PASS') . "\n";
 echo "</pre>";
+
+
+$headers = getallheaders();
+
+
+foreach($headers as $name => $value){
+    echo $name . ': ' . $value . "<br>";
+}
 ?>
