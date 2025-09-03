18ec4530eb64082fb6069a66ae2a782104b20caa
Jacke1012 <jacob.jonasson2003@gmail.com>
2025-09-03 10:34:08 +0200

    remove debug

--- app/public/php/debug.php
+++ app/public/php/debug.php
@@ -6,14 +6,14 @@ echo "DB_NAME=" . getenv('DB_NAME') . "\n";
 //echo "DB_PASS=" . getenv('DB_PASS') . "\n";
 echo "</pre>";
 
-$userEmail = $_SERVER['HTTP_CF_ACCESS_AUTHENTICATED_USER_EMAIL'] ?? 'invalid';
+// $userEmail = $_SERVER['HTTP_CF_ACCESS_AUTHENTICATED_USER_EMAIL'] ?? 'invalid';
 
-echo $userEmail . "<br><br><br><br>";
+// echo $userEmail . "<br><br><br><br>";
 
-$headers = getallheaders();
+// $headers = getallheaders();
 
 
-foreach($headers as $name => $value){
-    echo $name . ': ' . $value . "<br>";
-}
+// foreach($headers as $name => $value){
+//     echo $name . ': ' . $value . "<br>";
+// }
 ?>
